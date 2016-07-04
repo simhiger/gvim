7976bfbe4e9f3dc55b6792b8ccf5d11247856808
Doron Gombosh <dorong@satixfy.lan>
2016-07-04 11:11:31 +0300

    fixed some windows issues

--- gvim_install_win.py
+++ gvim_install_win.py
@@ -9,7 +9,7 @@ if ans in yes:
     os.system(r'"C:\Program Files\Internet Explorer\iexplore.exe" https://git-scm.com/download/win')
 
 ans = raw_input("run gvim in windows, and execute ':echo $HOME', input the path here(usualy C:\Users... or U:\ ):")
-os.system(r"git clone https://github.com/gombosh/dotvim.git %s\vimfiles"%ans)
+os.system(r'"C:\Program Files\Git\bin\git.exe" clone https://github.com/gombosh/dotvim.git %s\vimfiles'%ans)
 os.chdir(r"%s\vimfiles"%ans)
-os.system(r"git submodule update --init --recursive")
+os.system(r'"C:\Program Files\Git\bin\git.exe" submodule update --init --recursive')
 raw_input("Install finished, you can create a %s\myvimrc file to add your own commands and settings. press enter to close the installer"%ans)
