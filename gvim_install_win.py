f344f117171b4dc42852207484559f3984054ccd
LAB A <lab-A@satixfy.lan>
2016-11-22 13:46:50 +0200

    fixed path

--- gvim_install_win.py
+++ gvim_install_win.py
@@ -8,8 +8,12 @@ ans = raw_input("Do you want to install git for windows?")
 if ans in yes:
     os.system(r'"C:\Program Files\Internet Explorer\iexplore.exe" https://git-scm.com/download/win')
 
-ans = raw_input("run gvim in windows, and execute ':echo $HOME', input the path here(usualy C:\Users... or U:\ ):")
+homedir = raw_input("run gvim in windows, and execute ':echo $HOME', input the path here(usualy C:\Users... or U:\ ):")
 os.system(r'"C:\Program Files\Git\bin\git.exe" clone https://github.com/gombosh/dotvim.git %s\vimfiles'%ans)
-os.chdir(r"%s\vimfiles"%ans)
+os.chdir(r"%s\vimfiles"%homedir)
 os.system(r'"C:\Program Files\Git\bin\git.exe" submodule update --init --recursive')
+ans = raw_input("Set PATH for gvim tools?")
+if ans in yes:
+    os.getenv("PATH")
+    os.system(r'setx PATH "%PATH%;%s\vimfiles\bin"'%homedir)
 raw_input("Install finished, you can create a %s\myvimrc file to add your own commands and settings. press enter to close the installer"%ans)
